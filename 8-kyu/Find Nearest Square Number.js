f3f1aa073243db7f9f6a0ce5456e38cba847dcb4
satyadvaya <satyadvaya@gmail.com>
2022-06-12 22:14:05 -0700

    added 8kyu Find Nearest Square Number

---
+++ 8-kyu/Find Nearest Square Number.js
@@ -0,0 +1,22 @@
+// DESCRIPTION:
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+// Alphabetically ordered
+// Case-sensitive!
+// Not prime numbers
+
+function nearestSq(n) {
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+
+// describe("Basic tests",() => {
+//   Test.assertEquals(nearestSq(1),1)
+//   Test.assertEquals(nearestSq(2),1)
+//   Test.assertEquals(nearestSq(10),9)
+//   Test.assertEquals(nearestSq(111),121)
+//   Test.assertEquals(nearestSq(9999),10000)
+// });
