fe9fa74920cb6dc7ad248dd35915d43960aeff63
satyadvaya <satyadvaya@gmail.com>
2022-06-27 12:46:51 -0700

    added 8kyu Potenciation

---
+++ 8-kyu/Potenciation.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+// Great coding <3
+
+function power(x, y) {
+  return x ** y;
+}
+
+// describe("Tests", function(){
+
+// Test.assertEquals(power(1,701270), 1);
+// Test.assertEquals(power(2,2), 4);
+// Test.assertEquals(power(3,2), 9);
+// Test.assertEquals(power(-1,40), 1);
+
+// });
