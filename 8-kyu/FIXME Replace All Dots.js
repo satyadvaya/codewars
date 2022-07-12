628b07ee14c0ebafac98eea43d2ac9b8dea0a8e8
satyadvaya <satyadvaya@gmail.com>
2022-07-12 10:21:33 -0700

    added 8kyu FIXME Replace All Dots

---
+++ 8-kyu/FIXME Replace All Dots.js
@@ -0,0 +1,34 @@
+DESCRIPTION:
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+
+var replaceDots = function(str) {
+  return str.split('.').join('-');
+}
+
+var replaceDots = function(str) {
+  return str.replace(/[\.\/]/g, '-');
+}
+
+var replaceDots = function(str) {
+  return str.replace(/[.]/g, '-');
+}
+
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
+}
+
+describe("Example Tests", function() {
+
+  it("test dots", function(){
+    Test.assertEquals(replaceDots("one.two.three"), "one-two-three", "Sorry, try again :-(");
+  });
+  
+});
