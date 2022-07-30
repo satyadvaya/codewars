9d8aef29d9f24bc49ea7b7a5816b1adec6dffb0d
satyadvaya <satyadvaya@gmail.com>
2022-07-29 23:58:01 -0700

    added 8kyu Difference of Volumes of Cuboids

---
+++ 8-kyu/Difference of Volumes of Cuboids.js
@@ -0,0 +1,31 @@
+// DESCRIPTION:
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code.
+
+// eslint-disable-next-line no-unused-vars
+function findDifference(a, b) {
+  return Math.abs(a.reduce((x, y) => x * y) - b.reduce((x, y) => x * y));
+}
+
+// function findDifference(a, b) {
+//   return Math.abs( (a[0]*a[1]*a[2]) - (b[0]*b[1]*b[2]) );
+// }
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", function(){
+//   it("Basic tests", function(){
+//     assert.strictEqual(findDifference([3, 2, 5], [1, 4, 4]), 14);
+//     assert.strictEqual(findDifference([9, 7, 2], [5, 2, 2]), 106);
+//     assert.strictEqual(findDifference([11, 2, 5], [1, 10, 8]), 30);
+//     assert.strictEqual(findDifference([4, 4, 7], [3, 9, 3]), 31);
+//     assert.strictEqual(findDifference([15, 20, 25], [10, 30, 25]), 0);
+//   });
+// });
