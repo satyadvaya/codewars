17fccdb2bfac14a11142cd0a1ec5e870a524b219
satyadvaya <satyadvaya@gmail.com>
2022-07-29 23:53:41 -0700

    added 8kyu Find out Whether the Shape Is a Cube

--- 8-kyu/Do You Speak English.js
+++ 8-kyu/Do You Speak English.js
@@ -1,19 +1,20 @@
-DESCRIPTION:
-Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+// DESCRIPTION:
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
 
-The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
 
-Upper or lower case letter does not matter -- "eNglisH" is also correct.
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
 
-Return value as boolean values, true for the string to contains "English", false for it does not.
+// Return value as boolean values, true for the string to contains "English", false for it does not.
 
+// eslint-disable-next-line no-unused-vars
 function spEng(sentence) {
-  return sentence.toLowerCase().includes('english');
+  return sentence.toLowerCase().includes("english");
 }
 
-describe("Tests", () => {
-  it("test", () => {
-Test.assertEquals(spEng("english"), true);
-Test.assertEquals(spEng("egnlish"), false);
-  });
-});
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(spEng("english"), true);
+// Test.assertEquals(spEng("egnlish"), false);
+//   });
+// });
