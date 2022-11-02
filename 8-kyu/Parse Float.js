69a58c4f1cfe699871774c396b893903762293c0
satyadvaya <satyadvaya@gmail.com>
2022-11-02 10:12:59 -0700

    added 8kyu Parse Float

---
+++ 8-kyu/Parse Float.js
@@ -0,0 +1,14 @@
+// Description:
+
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+// eslint-disable-next-line no-unused-vars
+function parseF(s) {
+  return Number.isNaN(parseFloat(s)) ? null : parseFloat(s);
+}
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(parseF("1"), 1.0);
+//   });
+// });
