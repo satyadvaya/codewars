dcdb7b6ae62974f1caac2038ecc5fb1cfd1e44de
satyadvaya <satyadvaya@gmail.com>
2022-11-08 14:26:54 -0800

    added 8kyu Powers of 2

---
+++ 8-kyu/Powers of 2.js
@@ -0,0 +1,29 @@
+// Description:
+
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+// Examples
+
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+// eslint-disable-next-line no-unused-vars
+function powersOfTwo(n) {
+  const array = [];
+  for (let i = 0; i <= n; i++) {
+    array.push(2 ** i);
+  }
+  return array;
+}
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic Tests", function(){
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(powersOfTwo(0), [1])
+//     assert.deepEqual(powersOfTwo(1), [1, 2])
+//     assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16])
+//   })
+// });
