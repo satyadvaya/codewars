ad3d19a4456df04671a2a694ee7b882830d9ab9e
satyadvaya <satyadvaya@gmail.com>
2023-01-26 02:45:29 -0800

    added 8kyu Not So Black Box

---
+++ 8-kyu/Not So Black Box.js
@@ -0,0 +1,18 @@
+// Description:
+
+// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+
+// You may need to think laterally!
+
+// eslint-disable-next-line no-undef
+console.log(blackBox.toString());
+// if("you call JSopenSesame(), you'll pass!");
+
+// eslint-disable-next-line no-undef
+JSopenSesame();
+
+// describe("Did you open the box?", function() {
+
+//   Test.expect(check(), "It's clamped shut!");
+
+// });
