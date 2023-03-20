3e99e5fa9281ed033a6ebd4d04bd495db33003d6
satyadvaya <satyadvaya@gmail.com>
2023-03-19 23:00:43 -0700

    refactored challenge

--- 8-kyu/Abbreviate a Two Word Name.js
+++ 8-kyu/Abbreviate a Two Word Name.js
@@ -1,13 +1,15 @@
-// Description:
+/*
+Description:
 
-// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
 
-// The output should be two capital letters with a dot separating them.
+The output should be two capital letters with a dot separating them.
 
-// It should look like this:
+It should look like this:
 
-// Sam Harris => S.H
-// patrick feeney => P.F
+Sam Harris => S.H
+patrick feeney => P.F
+*/
 
 // eslint-disable-next-line no-unused-vars
 function abbrevName(name) {
@@ -17,22 +19,30 @@ function abbrevName(name) {
     .join(".");
 }
 
-// function abbrevName(name) {
-//   return name.split(' ').map(word => word.slice(0, 1).toUpperCase()).join('.');
-// }
+/*
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((word) => word.slice(0, 1).toUpperCase())
+    .join(".");
+}
 
-// function abbrevName(name) {
-//   return name.split(' ').map(word => word.substring(0, 1).toUpperCase()).join('.');
-// }
+function abbrevName(name) {
+  return name
+    .split(" ")
+    .map((word) => word.substring(0, 1).toUpperCase())
+    .join(".");
+}
 
-// chai.config.truncateThreshold=0;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests", () => {
-//   it("Testing for fixed tests", () => {
-//     assert.strictEqual(abbrevName("Sam Harris"), "S.H");
-//     assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
-//     assert.strictEqual(abbrevName("Evan Cole"), "E.C");
-//     assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
-//     assert.strictEqual(abbrevName("David Mendieta"), "D.M");
-//   });
-// });
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(abbrevName("Sam Harris"), "S.H");
+    assert.strictEqual(abbrevName("Patrick Feenan"), "P.F");
+    assert.strictEqual(abbrevName("Evan Cole"), "E.C");
+    assert.strictEqual(abbrevName("P Favuzzi"), "P.F");
+    assert.strictEqual(abbrevName("David Mendieta"), "D.M");
+  });
+});
+*/
