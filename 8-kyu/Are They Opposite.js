2b1d449fe171bb13531dec1ff33e526cf3864afe
satyadvaya <satyadvaya@gmail.com>
2023-03-22 17:00:00 -0700

    refactored challenge

--- 8-kyu/Are They Opposite.js
+++ 8-kyu/Are They Opposite.js
@@ -1,16 +1,18 @@
-// Description:
-// Task
+/*
+Description:
+Task
 
-// Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
+Give you two strings: s1 and s2. If they are opposite, return true; otherwise, return false. Note: The result should be a boolean value, instead of a string.
 
-// The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
-// Examples (input -> output)
+The opposite means: All letters of the two strings are the same, but the case is opposite. you can assume that the string only contains letters or it's a empty string. Also take note of the edge case - if both strings are empty then you should return false/False.
+Examples (input -> output)
 
-// "ab","AB"     -> true
-// "aB","Ab"     -> true
-// "aBcd","AbCD" -> true
-// "AB","Ab"     -> false
-// "",""         -> false
+"ab","AB"     -> true
+"aB","Ab"     -> true
+"aBcd","AbCD" -> true
+"AB","Ab"     -> false
+"",""         -> false
+*/
 
 // eslint-disable-next-line no-unused-vars
 function isOpposite(s1, s2) {
@@ -23,14 +25,15 @@ function isOpposite(s1, s2) {
   return true;
 }
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Test.assertSimilar(isOpposite("ab","AB") , true);
-// Test.assertSimilar(isOpposite("aB","Ab") , true);
-// Test.assertSimilar(isOpposite("aBcd","AbCD") , true);
-// Test.assertSimilar(isOpposite("aBcde","AbCD") , false);
-// Test.assertSimilar(isOpposite("AB","Ab") , false);
-// Test.assertSimilar(isOpposite("","") , false);
-
-//   });
-// });
+/*
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertSimilar(isOpposite("ab", "AB"), true);
+    Test.assertSimilar(isOpposite("aB", "Ab"), true);
+    Test.assertSimilar(isOpposite("aBcd", "AbCD"), true);
+    Test.assertSimilar(isOpposite("aBcde", "AbCD"), false);
+    Test.assertSimilar(isOpposite("AB", "Ab"), false);
+    Test.assertSimilar(isOpposite("", ""), false);
+  });
+});
+*/
