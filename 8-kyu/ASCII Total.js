45791bd4e8c22c166b4f826c33a4e85c195924cf
satyadvaya <satyadvaya@gmail.com>
2023-03-25 12:00:00 -0700

    refactored challenge

--- 8-kyu/ASCII Total.js
+++ 8-kyu/ASCII Total.js
@@ -1,9 +1,7 @@
 /*Description:
-
 You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all ASCII characters.
 
 examples:
-
 uniTotal("a") == 97 uniTotal("aaa") == 291
 */
 
