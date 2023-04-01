57c37194863c452980c26ce5c3827929737684c8
satyadvaya <satyadvaya@gmail.com>
2023-04-01 11:00:00 -0700

    refactored challenge

--- 8-kyu/Be Concise IV Index of an Element in an Array.js
+++ 8-kyu/Be Concise IV Index of an Element in an Array.js
@@ -1,32 +1,36 @@
-// Description:
-// Be Concise IV - Index of an element in an array
-// Task
+/*
+Description:
+Be Concise IV - Index of an element in an array
+Task
 
-// Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+Provided is a function find which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise. Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. (no more than 85) You may assume that all array elements are unique.
+*/
 
 // eslint-disable-next-line no-unused-vars
 function find(ar, el) {
   return ar.includes(el) ? ar.indexOf(el) : "Not found";
 }
 
-// describe("Your refactored find() function", () => {
-//   it("should behave as expected", () => {
-//     var array = [2,3,5,7,11];
-//     Test.assertEquals(find(array, 5), 2);
-//     Test.assertEquals(find(array, 11), 4);
-//     Test.assertEquals(find(array, 3), 1);
-//     Test.assertEquals(find(array, 2), 0);
-//     Test.assertEquals(find(array, 7), 3);
-//     Test.assertEquals(find(array, 1), "Not found");
-//     Test.assertEquals(find(array, 8), "Not found");
-//     array = [true, "Hello World", false, "Lorem Ipsum", 6, Math.PI];
-//     Test.assertEquals(find(array, "Hello World"), 1);
-//     Test.assertEquals(find(array, "lorem ipsum"), "Not found");
-//     Test.assertEquals(find(array, "Lorem Ipsum"), 3);
-//     Test.assertEquals(find(array, false), 2);
-//     Test.assertEquals(find(array, true), 0);
-//     Test.assertEquals(find(array, Math.PI), 5);
-//     Test.assertEquals(find(array, 3.14), "Not found");
-//     Test.assertEquals(find(array, 6), 4);
-//   });
-// });
+/*
+describe("Your refactored find() function", () => {
+  it("should behave as expected", () => {
+    var array = [2, 3, 5, 7, 11];
+    Test.assertEquals(find(array, 5), 2);
+    Test.assertEquals(find(array, 11), 4);
+    Test.assertEquals(find(array, 3), 1);
+    Test.assertEquals(find(array, 2), 0);
+    Test.assertEquals(find(array, 7), 3);
+    Test.assertEquals(find(array, 1), "Not found");
+    Test.assertEquals(find(array, 8), "Not found");
+    array = [true, "Hello World", false, "Lorem Ipsum", 6, Math.PI];
+    Test.assertEquals(find(array, "Hello World"), 1);
+    Test.assertEquals(find(array, "lorem ipsum"), "Not found");
+    Test.assertEquals(find(array, "Lorem Ipsum"), 3);
+    Test.assertEquals(find(array, false), 2);
+    Test.assertEquals(find(array, true), 0);
+    Test.assertEquals(find(array, Math.PI), 5);
+    Test.assertEquals(find(array, 3.14), "Not found");
+    Test.assertEquals(find(array, 6), 4);
+  });
+});
+*/
