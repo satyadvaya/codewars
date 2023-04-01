c0de5e572afc2b6068bb6efecea703b428b01223
satyadvaya <satyadvaya@gmail.com>
2023-04-01 14:00:00 -0700

    refactored challenge

--- 8-kyu/Bin to Decimal.js
+++ 8-kyu/Bin to Decimal.js
@@ -1,11 +1,15 @@
-// DESCRIPTION:
-// Complete the function which converts a binary number (given as a string) to a decimal number.
+/*
+DESCRIPTION:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+*/
 
 // eslint-disable-next-line no-unused-vars
 function binToDec(bin) {
   return parseInt(bin, 2);
 }
 
-// describe("Example Tests", () => {
-//   [ ["1",1], ["0",0], ["1001001", 73] ].forEach( ([inp, exp]) => Test.assertEquals( binToDec(inp), exp ) )
-// });
+/*
+describe("Example Tests", () => {
+  [ ["1",1], ["0",0], ["1001001", 73] ].forEach( ([inp, exp]) => Test.assertEquals( binToDec(inp), exp ) )
+});
+*/
