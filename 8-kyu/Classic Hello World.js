dba61c717bfa59d18b078772c2baa3fcc18a00e0
satyadvaya <satyadvaya@gmail.com>
2023-04-07 10:00:00 -0700

    refactored challenge

--- 8-kyu/Classic Hello World.js
+++ 8-kyu/Classic Hello World.js
@@ -1,19 +1,21 @@
-// Description:
+/*
+Description:
 
-// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
 
-// Note that for some languages, the function main is the entry point of the program.
+Note that for some languages, the function main is the entry point of the program.
 
-// Here's how it will be tested:
+Here's how it will be tested:
 
-//     Solution.main("parameter1","parameter2");
+    Solution.main("parameter1","parameter2");
 
-// Hints:
+Hints:
 
-//     Check your references
-//     Think about the scope of your method
-//     For prolog you can use write but there are better ways
-//     If you still don't get it probably you can define main as an attribute of the Solution class that accepts a single argument, and that only prints "Hello World!" without any return.
+    Check your references
+    Think about the scope of your method
+    For prolog you can use write but there are better ways
+    If you still don't get it probably you can define main as an attribute of the Solution class that accepts a single argument, and that only prints "Hello World!" without any return.
+*/
 
 // eslint-disable-next-line no-unused-vars
 class Solution {
@@ -22,11 +24,13 @@ class Solution {
   }
 }
 
-// const Test = require('@codewars/test-compat');
+/*
+const Test = require("@codewars/test-compat");
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Solution.main("greetings from Javatlacati!");
-// Test.expect(logRecord.indexOf("Hello World!") !== -1);
-//   });
-// });
+describe("Tests", () => {
+  it("test", () => {
+    Solution.main("greetings from Javatlacati!");
+    Test.expect(logRecord.indexOf("Hello World!") !== -1);
+  });
+});
+*/
