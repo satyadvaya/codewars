23d100b371c8c849c0f47706338ad7f404b1dd71
satyadvaya <satyadvaya@gmail.com>
2023-04-07 18:00:00 -0700

    refactored challenge

--- 8-kyu/Convert a String to an Array.js
+++ 8-kyu/Convert a String to an Array.js
@@ -1,24 +1,36 @@
-// Description:
+/*
+Description:
 
-// Write a function to split a string and convert it into an array of words.
-// Examples (Input ==> Output):
+Write a function to split a string and convert it into an array of words.
+Examples (Input ==> Output):
 
-// "Robin Singh" ==> ["Robin", "Singh"]
+"Robin Singh" ==> ["Robin", "Singh"]
 
-// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+*/
 
 // eslint-disable-next-line no-unused-vars
 function stringToArray(string) {
   return string.split(" ");
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests", () => {
-//   it("Testing for fixed tests", () => {
-//     assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
-//     assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
-//   });
-// });
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+    assert.deepEqual(stringToArray("I love arrays they are my favorite"), [
+      "I",
+      "love",
+      "arrays",
+      "they",
+      "are",
+      "my",
+      "favorite",
+    ]);
+  });
+});
+*/
