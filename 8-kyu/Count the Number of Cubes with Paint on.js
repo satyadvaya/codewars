98f084a1e39d91a43f58eae0a36f5390c2e5c97a
satyadvaya <satyadvaya@gmail.com>
2023-04-10 23:03:43 -0700

    refactored challenge

--- 8-kyu/Count the Number of Cubes with Paint on.js
+++ 8-kyu/Count the Number of Cubes with Paint on.js
@@ -1,14 +1,16 @@
-// Description:
+/*
+Description:
 
-// Upon arriving at an interview, you are presented with a solid blue cube. The cube is then dipped in red paint, coating the entire surface of the cube. The interviewer then proceeds to cut through the cube in all three dimensions a certain number of times.
+Upon arriving at an interview, you are presented with a solid blue cube. The cube is then dipped in red paint, coating the entire surface of the cube. The interviewer then proceeds to cut through the cube in all three dimensions a certain number of times.
 
-// Your function takes as parameter the number of times the cube has been cut. You must return the number of smaller cubes created by the cuts that have at least one red face.
+Your function takes as parameter the number of times the cube has been cut. You must return the number of smaller cubes created by the cuts that have at least one red face.
 
-// To make it clearer, the picture below represents the cube after (from left to right) 0, 1 and 2 cuts have been made.
+To make it clearer, the picture below represents the cube after (from left to right) 0, 1 and 2 cuts have been made.
 
-// Examples:
-// countSquares(2) --> 26
-// countSquares(4) --> 98
+Examples:
+countSquares(2) --> 26
+countSquares(4) --> 98
+*/
 
 // eslint-disable-next-line no-unused-vars
 const countSquares = function (cuts) {
@@ -22,8 +24,10 @@ const countSquares = function (cuts) {
   return cuts === 0 ? 1 : 6 * Math.pow(cuts, 2) + 2;
 };
 
-// describe("Basic tests",() =>{
-// Test.assertSimilar(countSquares(5),152)
-// Test.assertSimilar(countSquares(16),1538)
-// Test.assertSimilar(countSquares(23),3176)
-// })
+/*
+describe("Basic tests", () => {
+  Test.assertSimilar(countSquares(5), 152);
+  Test.assertSimilar(countSquares(16), 1538);
+  Test.assertSimilar(countSquares(23), 3176);
+});
+*/
