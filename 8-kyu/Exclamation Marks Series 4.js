132acda80ca86001604c43990ce8d0a711f242eb
satyadvaya <satyadvaya@gmail.com>
2023-04-15 12:00:00 -0700

    refactored challenge

--- 8-kyu/Exclamation Marks Series 4.js
+++ 8-kyu/Exclamation Marks Series 4.js
@@ -1,14 +1,16 @@
-// Description:
+/*
+Description:
 
-// Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
-// Examples
+Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+Examples
 
-// remove("Hi!") === "Hi!"
-// remove("Hi!!!") === "Hi!"
-// remove("!Hi") === "Hi!"
-// remove("!Hi!") === "Hi!"
-// remove("Hi! Hi!") === "Hi Hi!"
-// remove("Hi") === "Hi!"
+remove("Hi!") === "Hi!"
+remove("Hi!!!") === "Hi!"
+remove("!Hi") === "Hi!"
+remove("!Hi!") === "Hi!"
+remove("Hi! Hi!") === "Hi Hi!"
+remove("Hi") === "Hi!"
+*/
 
 // eslint-disable-next-line no-unused-vars
 function remove(string) {
@@ -20,24 +22,26 @@ function remove(string) {
   );
 }
 
-// function remove (string) {
-//   return string.split('!').join('').concat('!');
-// }
+/*
+function remove(string) {
+  return string.split("!").join("").concat("!");
+}
 
-// const strictEqual = require("chai").assert.strictEqual;
+const strictEqual = require("chai").assert.strictEqual;
 
-// function doTest (input, expected) {
-// 	const actual = remove(input);
-// 	strictEqual(actual, expected, `for string:\n"${input}"\n`);
-// }
+function doTest(input, expected) {
+  const actual = remove(input);
+  strictEqual(actual, expected, `for string:\n"${input}"\n`);
+}
 
-// describe("Tests suite", function () {
-// 	it("sample tests", function () {
-// 		doTest("Hi!", "Hi!");
-// 		doTest("Hi!!!", "Hi!");
-// 		doTest("!Hi", "Hi!");
-// 		doTest("!Hi!", "Hi!");
-// 		doTest("Hi! Hi!", "Hi Hi!");
-// 		doTest("Hi", "Hi!");
-// 	});
-// });
+describe("Tests suite", function () {
+  it("sample tests", function () {
+    doTest("Hi!", "Hi!");
+    doTest("Hi!!!", "Hi!");
+    doTest("!Hi", "Hi!");
+    doTest("!Hi!", "Hi!");
+    doTest("Hi! Hi!", "Hi Hi!");
+    doTest("Hi", "Hi!");
+  });
+});
+*/
