882e382d7b1d081341986341da6cbaf9aa861f5a
satyadvaya <satyadvaya@gmail.com>
2023-04-15 11:00:00 -0700

    refactored challenge

--- 8-kyu/Exclamation Marks Series 2.js
+++ 8-kyu/Exclamation Marks Series 2.js
@@ -1,14 +1,16 @@
-// Description:
+/*
+Description:
 
-// Remove all exclamation marks from the end of sentence.
-// Examples
+Remove all exclamation marks from the end of sentence.
+Examples
 
-// remove("Hi!") === "Hi"
-// remove("Hi!!!") === "Hi"
-// remove("!Hi") === "!Hi"
-// remove("!Hi!") === "!Hi"
-// remove("Hi! Hi!") === "Hi! Hi"
-// remove("Hi") === "Hi"
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+*/
 
 // eslint-disable-next-line no-unused-vars
 function remove(string) {
@@ -18,28 +20,30 @@ function remove(string) {
   return string;
 }
 
-// function remove (string) {
-//   let array = [];
-//   for (let i = string.length-1; i > 0; i--) {
-//     if (string[i] != '!') {
-//       array.push(string.slice(0, i+1));
-//       return array.toString();
-//     }
-//   }
-// }
+/*
+function remove(string) {
+  let array = [];
+  for (let i = string.length - 1; i > 0; i--) {
+    if (string[i] != "!") {
+      array.push(string.slice(0, i + 1));
+      return array.toString();
+    }
+  }
+}
 
-// const strictEqual = require("chai").assert.strictEqual;
+const strictEqual = require("chai").assert.strictEqual;
 
-// function doTest (input, expected) {
-// 	const actual = remove(input);
-// 	strictEqual(actual, expected, `for string:\n"${input}"\n`);
-// }
+function doTest(input, expected) {
+  const actual = remove(input);
+  strictEqual(actual, expected, `for string:\n"${input}"\n`);
+}
 
-// it("It should works for basic tests", function () {
-// 	doTest("Hi!", "Hi")
-// 	doTest("Hi!!!", "Hi")
-// 	doTest("!Hi", "!Hi")
-// 	doTest("!Hi!", "!Hi")
-// 	doTest("Hi! Hi!", "Hi! Hi")
-// 	doTest("Hi", "Hi")
-// });
+it("It should works for basic tests", function () {
+  doTest("Hi!", "Hi");
+  doTest("Hi!!!", "Hi");
+  doTest("!Hi", "!Hi");
+  doTest("!Hi!", "!Hi");
+  doTest("Hi! Hi!", "Hi! Hi");
+  doTest("Hi", "Hi");
+});
+*/
