0a1cbb2ce4fd886971939cbfb3a3df37db3e069b
satyadvaya <satyadvaya@gmail.com>
2023-04-15 14:00:00 -0700

    refactored challenge

--- 8-kyu/Exclamation Marks Series 11.js
+++ 8-kyu/Exclamation Marks Series 11.js
@@ -1,29 +1,31 @@
-// Description:
-// Description:
+/*
+Description:
 
-// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
-// Examples
+Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+Examples
 
-// replace("Hi!") === "H!!"
-// replace("!Hi! Hi!") === "!H!! H!!"
-// replace("aeiou") === "!!!!!"
-// replace("ABCDE") === "!BCD!"
+replace("Hi!") === "H!!"
+replace("!Hi! Hi!") === "!H!! H!!"
+replace("aeiou") === "!!!!!"
+replace("ABCDE") === "!BCD!"
+*/
 
 // eslint-disable-next-line no-unused-vars
 function replace(s) {
   return s.replace(/[aeiou]/gi, "!");
 }
 
-// function replace(s) {
-//   return s.replace(/[aeiouAEIOU]/g, '!');
-// }
+/*
+function replace(s) {
+  return s.replace(/[aeiouAEIOU]/g, "!");
+}
 
-// describe("Basic Tests", function(){
-//   it("It should works for basic tests", function(){
-
-// Test.assertSimilar(replace("Hi!") , "H!!")
-// Test.assertSimilar(replace("!Hi! Hi!") , "!H!! H!!")
-// Test.assertSimilar(replace("aeiou") , "!!!!!")
-// Test.assertSimilar(replace("ABCDE") , "!BCD!")
-
-// })})
+describe("Basic Tests", function () {
+  it("It should works for basic tests", function () {
+    Test.assertSimilar(replace("Hi!"), "H!!");
+    Test.assertSimilar(replace("!Hi! Hi!"), "!H!! H!!");
+    Test.assertSimilar(replace("aeiou"), "!!!!!");
+    Test.assertSimilar(replace("ABCDE"), "!BCD!");
+  });
+});
+*/
