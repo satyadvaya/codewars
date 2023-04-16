cd0a4960f93ea274c9136cab341d85a36043e6f4
satyadvaya <satyadvaya@gmail.com>
2023-04-15 17:00:00 -0700

    refactored challenge

--- 8-kyu/Fake Binary.js
+++ 8-kyu/Fake Binary.js
@@ -1,8 +1,10 @@
-// Description:
+/*
+Description:
 
-// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 
-// Note: input will never be an empty string
+Note: input will never be an empty string
+*/
 
 // eslint-disable-next-line no-unused-vars
 function fakeBin(x) {
@@ -12,14 +14,19 @@ function fakeBin(x) {
     .join("");
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests",() =>{
-//   it("Testing for fixed tests", () => {
-//     assert.strictEqual(fakeBin('45385593107843568'), '01011110001100111');
-//     assert.strictEqual(fakeBin('509321967506747'), '101000111101101');
-//     assert.strictEqual(fakeBin('366058562030849490134388085'), '011011110000101010000011011');
-//   })
-// });
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(fakeBin("45385593107843568"), "01011110001100111");
+    assert.strictEqual(fakeBin("509321967506747"), "101000111101101");
+    assert.strictEqual(
+      fakeBin("366058562030849490134388085"),
+      "011011110000101010000011011"
+    );
+  });
+});
+*/
