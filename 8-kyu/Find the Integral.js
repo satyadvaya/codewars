8485495a1bea0a2dc04787fd79c8d6811f83853e
satyadvaya <satyadvaya@gmail.com>
2023-04-17 11:00:00 -0700

    refactored challenge

--- 8-kyu/Find the Integral.js
+++ 8-kyu/Find the Integral.js
@@ -1,3 +1,4 @@
+/*
 DESCRIPTION:
 Create a function that finds the integral of the expression passed.
 
@@ -15,21 +16,25 @@ Examples
 20, 1  -->  "10x^2"
 40, 3  -->  "10x^4"
 90, 2  -->  "30x^3"
+*/
 
+// eslint-disable-next-line no-unused-vars
 function integrate(coefficient, exponent) {
   return `${coefficient / (exponent + 1)}x^${exponent + 1}`;
 }
 
+/*
 function integrate(coefficient, exponent) {
-  return (coefficient / (exponent + 1)) + 'x^' + (exponent + 1);
+  return coefficient / (exponent + 1) + "x^" + (exponent + 1);
 }
 
 describe("Tests", () => {
   it("test", () => {
-Test.assertEquals(integrate(3,2), "1x^3");
-Test.assertEquals(integrate(12,5), "2x^6");
-Test.assertEquals(integrate(20,1), "10x^2")
-Test.assertEquals(integrate(40,3), "10x^4")
-Test.assertEquals(integrate(90,2), "30x^3")
+    Test.assertEquals(integrate(3, 2), "1x^3");
+    Test.assertEquals(integrate(12, 5), "2x^6");
+    Test.assertEquals(integrate(20, 1), "10x^2");
+    Test.assertEquals(integrate(40, 3), "10x^4");
+    Test.assertEquals(integrate(90, 2), "30x^3");
   });
 });
+*/
