56980b4e774123fbcdbbb82339ffc12752e3454e
satyadvaya <satyadvaya@gmail.com>
2023-04-19 23:49:57 -0700

    refactored challenge

--- 8-kyu/Ensure Question.js
+++ 8-kyu/Ensure Question.js
@@ -1,24 +1,28 @@
-// DESCRIPTION:
-// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+/*
+DESCRIPTION:
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
 
-// For example (Input --> Output)
+For example (Input --> Output)
 
-// "Yes" --> "Yes?"
-// "No?" --> "No?"
+"Yes" --> "Yes?"
+"No?" --> "No?"
+*/
 
 // eslint-disable-next-line no-unused-vars
 function ensureQuestion(s) {
   return s.endsWith("?") ? s : s + "?";
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Ensure question", () => {
-//   it("Fixed tests", () => {
-//     assert.strictEqual(ensureQuestion(""),"?","Expected: '?'");
-//     assert.strictEqual(ensureQuestion("Yes"),"Yes?","Expected: '?'");
-//     assert.strictEqual(ensureQuestion("No?"),"No?","Expected: '?'");
-//   });
-// });
+describe("Ensure question", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(ensureQuestion(""), "?", "Expected: '?'");
+    assert.strictEqual(ensureQuestion("Yes"), "Yes?", "Expected: '?'");
+    assert.strictEqual(ensureQuestion("No?"), "No?", "Expected: '?'");
+  });
+});
+*/
