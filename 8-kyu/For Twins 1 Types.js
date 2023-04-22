5e932f63291cfdec4e01aaa621495969fa09d7fa
satyadvaya <satyadvaya@gmail.com>
2023-04-22 14:00:00 -0700

    refactored challenge

--- 8-kyu/For Twins 1 Types.js
+++ 8-kyu/For Twins 1 Types.js
@@ -1,3 +1,4 @@
+/*
 DESCRIPTION:
 Prolog:
 This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
@@ -8,12 +9,16 @@ Write function typeValidation that will accept two parameters: variable and type
 Examples:
 typeValidation(42, "number");   // => true
 typeValidation("42", "number"); // => false
+*/
 
+// eslint-disable-next-line no-unused-vars
 function typeValidation(variable, type) {
-  return (typeof variable) == type;
+  return typeof variable == type;
 }
 
-describe("Basic Tests", function() {
+/*
+describe("Basic Tests", function () {
   Test.assertEquals(typeValidation(42, "number"), true);
   Test.assertEquals(typeValidation("42", "number"), false);
 });
+*/
