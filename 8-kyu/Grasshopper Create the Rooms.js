30b33187a7ab4b23a58acf3a94f6fbc82447a43b
satyadvaya <satyadvaya@gmail.com>
2023-04-25 15:27:46 -0700

    refactored challenge

--- 8-kyu/Grasshopper Create the Rooms.js
+++ 8-kyu/Grasshopper Create the Rooms.js
@@ -1,7 +1,9 @@
-// Description:
-// Escape the room
+/*
+Description:
+Escape the room
 
-// You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+*/
 
 // eslint-disable-next-line no-unused-vars
 var rooms = {
@@ -10,22 +12,27 @@ var rooms = {
   room3: { name: "name 3", description: "description 3", completed: true },
 };
 
-// const Test = require('@codewars/test-compat');
+/*
+const Test = require("@codewars/test-compat");
 
-// describe('room creation', () => {
-//   const keys = Object.keys(rooms)
-//   it('should have at least three rooms', () => {
-//     Test.assertEquals(keys.length >= 3, true)
-//   })
-//   it('each room should be an object', () => {
-//     keys.forEach(name => {
-//       Test.expect(typeof(rooms[name]) === 'object', `${name} should be an object`)
-//     })
-//   })
-//   it('should contain at least three properties per room', () => {
-//     keys.forEach(name => {
-//       let numKeys = Object.keys(rooms[name]).length
-//       Test.expect(numKeys >= 3, `not enough properties for room: ${name}`)
-//     })
-//   })
-// })
+describe("room creation", () => {
+  const keys = Object.keys(rooms);
+  it("should have at least three rooms", () => {
+    Test.assertEquals(keys.length >= 3, true);
+  });
+  it("each room should be an object", () => {
+    keys.forEach((name) => {
+      Test.expect(
+        typeof rooms[name] === "object",
+        `${name} should be an object`
+      );
+    });
+  });
+  it("should contain at least three properties per room", () => {
+    keys.forEach((name) => {
+      let numKeys = Object.keys(rooms[name]).length;
+      Test.expect(numKeys >= 3, `not enough properties for room: ${name}`);
+    });
+  });
+});
+*/
