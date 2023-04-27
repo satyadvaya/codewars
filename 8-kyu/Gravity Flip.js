e44f55d08ae4d27c0c1afd1d8d951eee6d5eab34
satyadvaya <satyadvaya@gmail.com>
2023-04-26 17:29:39 -0700

    refactored challenge

--- 8-kyu/Gravity Flip.js
+++ 8-kyu/Gravity Flip.js
@@ -1,34 +1,36 @@
-// If you've completed this kata already and want a bigger challenge, here's the 3D version
+/*
+Description:
 
-// Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
+If you've completed this kata already and want a bigger challenge, here's the 3D version
 
-// There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
+Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special because it has the ability to change gravity.
 
-// +---+                                       +---+
-// |   |                                       |   |
-// +---+                                       +---+
-// +---++---+     +---+              +---++---++---+
-// |   ||   |     |   |   -->        |   ||   ||   |
-// +---++---+     +---+              +---++---++---+
-// +---++---++---++---+         +---++---++---++---+
-// |   ||   ||   ||   |         |   ||   ||   ||   |
-// +---++---++---++---+         +---++---++---++---+
-// Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
+There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes. At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is an example of what a box of cubes might look like before and after switching gravity.
 
-// Examples (input -> output:
-// * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
-// * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
++---+                                       +---+
+|   |                                       |   |
++---+                                       +---+
++---++---+     +---+              +---++---++---+
+|   ||   |     |   |   -->        |   ||   ||   |
++---++---+     +---+              +---++---++---+
++---++---++---++---+         +---++---++---++---+
+|   ||   ||   ||   |         |   ||   ||   ||   |
++---++---++---++---+         +---++---++---++---+
+Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns after Bob switches the gravity.
 
-// Answer 1:
+Examples (input -> output:
+* 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+* 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+*/
 
+// eslint-disable-next-line no-unused-vars
 const flip = (d, a) =>
   d === "L" ? a.sort((x, y) => y - x) : a.sort((x, y) => x - y);
 
-// Answer 2:
-
-// const flip = (d, a) => {
-//   return d === 'L' ? a.sort((x, y) => y - x) : a.sort((x, y) => x - y);
-// }
+/*
+const flip = (d, a) => {
+  return d === "L" ? a.sort((x, y) => y - x) : a.sort((x, y) => x - y);
+};
 
 // Since Node 10, we're using Mocha.
 // You can use `chai` for assertions.
@@ -46,3 +48,4 @@ describe("Solution", function () {
     assert.deepEqual(flip("L", [1, 4, 5, 3, 5]), [5, 5, 4, 3, 1]);
   });
 });
+*/
