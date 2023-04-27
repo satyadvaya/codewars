bb3600637196f535f735f9597a1a855f98be9bc3
satyadvaya <satyadvaya@gmail.com>
2023-04-26 17:36:33 -0700

    refactored challenge

--- 8-kyu/Hello Name or World.js
+++ 8-kyu/Hello Name or World.js
@@ -1,15 +1,17 @@
-// Description:
+/*
+Description:
 
-// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
 
-// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
 
-// Examples:
+Examples:
 
-// * With `name` = "john"  => return "Hello, John!"
-// * With `name` = "aliCE" => return "Hello, Alice!"
-// * With `name` not given
-//   or `name` = ""        => return "Hello, World!"
+* With `name` = "john"  => return "Hello, John!"
+* With `name` = "aliCE" => return "Hello, Alice!"
+* With `name` not given
+  or `name` = ""        => return "Hello, World!"
+  */
 
 // eslint-disable-next-line no-unused-vars
 function hello(name) {
@@ -18,14 +20,35 @@ function hello(name) {
   }!`;
 }
 
-// const assert = require('chai').assert;
-// describe("Sample Tests", () => {
-//   it("should work for sample inputs", () => {
-//     let actualJohn = hello('johN');
-//     assert.isDefined(actualJohn, "You have to return a string instead of logging it to the console");
-//     assert.strictEqual(actualJohn, 'Hello, John!', "returns 'Hello, John!' when given 'johN'");
-//     assert.strictEqual(hello('alice'), 'Hello, Alice!', "returns 'Hello, Alice!' when given 'alice'");
-//     assert.strictEqual(hello(), 'Hello, World!', "returns 'Hello, World!' when name is not given");
-//     assert.strictEqual(hello(''), 'Hello, World!', "returns 'Hello, World!' when name is an empty string");
-//   });
-// });
+/*
+const assert = require("chai").assert;
+describe("Sample Tests", () => {
+  it("should work for sample inputs", () => {
+    let actualJohn = hello("johN");
+    assert.isDefined(
+      actualJohn,
+      "You have to return a string instead of logging it to the console"
+    );
+    assert.strictEqual(
+      actualJohn,
+      "Hello, John!",
+      "returns 'Hello, John!' when given 'johN'"
+    );
+    assert.strictEqual(
+      hello("alice"),
+      "Hello, Alice!",
+      "returns 'Hello, Alice!' when given 'alice'"
+    );
+    assert.strictEqual(
+      hello(),
+      "Hello, World!",
+      "returns 'Hello, World!' when name is not given"
+    );
+    assert.strictEqual(
+      hello(""),
+      "Hello, World!",
+      "returns 'Hello, World!' when name is an empty string"
+    );
+  });
+});
+*/
