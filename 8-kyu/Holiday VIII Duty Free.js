a08415ce1206896d5ae0605bf843fc35e01a7958
satyadvaya <satyadvaya@gmail.com>
2023-04-28 12:00:00 -0700

    refactored challenge

--- 8-kyu/Holiday VIII Duty Free.js
+++ 8-kyu/Holiday VIII Duty Free.js
@@ -1,26 +1,30 @@
-// Description:
+/*
+Description:
 
-// The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
+The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the saving over the normal high street price would effectively cover the cost of your holiday.
 
-// You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
+You will be given the high street price (normPrice), the duty free discount (discount) and the cost of the holiday.
 
-// For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
+For example, if a bottle cost £10 normally and the discount in duty free was 10%, you would save £1 per bottle. If your holiday cost £500, the answer you should return would be 500.
 
-// All inputs will be integers. Please return an integer. Round down.
+All inputs will be integers. Please return an integer. Round down.
+*/
 
 // eslint-disable-next-line no-unused-vars
 function dutyFree(normPrice, discount, hol) {
   return Math.floor(hol / (normPrice * (discount / 100)));
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests", () => {
-//   it("Testing for fixed tests", () => {
-//     assert.strictEqual(dutyFree(12, 50, 1000), 166);
-//     assert.strictEqual(dutyFree(17, 10, 500), 294);
-//     assert.strictEqual(dutyFree(24, 35, 3000), 357);
-//   });
-// })
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(dutyFree(12, 50, 1000), 166);
+    assert.strictEqual(dutyFree(17, 10, 500), 294);
+    assert.strictEqual(dutyFree(24, 35, 3000), 357);
+  });
+});
+*/
