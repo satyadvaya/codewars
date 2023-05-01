dea6bc700835be3946c277b9e2d281244d9669e3
satyadvaya <satyadvaya@gmail.com>
2023-05-01 16:03:49 -0700

    refactored challenge

--- 8-kyu/Improving Math.round(x).js
+++ 8-kyu/Improving Math.round(x).js
@@ -27,15 +27,17 @@ Math.roundTo = function (number, precision) {
   return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
 };
 
-// const Test = require('@codewars/test-compat');
+/*
+const Test = require("@codewars/test-compat");
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Test.assertEquals(Math.roundTo(3.1415926535, 4), 3.1416);
-// Test.assertEquals(Math.roundTo(4, 5), 4);
-// Test.assertEquals(Math.roundTo(4.1235343424, 6), 4.123534);
-// Test.assertEquals(Math.roundTo(5.3035802, 3), 5.304);
-// Test.assertEquals(Math.roundTo(0.9384930193848595, 15), 0.938493019384860);
-// Test.assertEquals(Math.roundTo(9.9999, 3), 10);
-//   });
-// });
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(Math.roundTo(3.1415926535, 4), 3.1416);
+    Test.assertEquals(Math.roundTo(4, 5), 4);
+    Test.assertEquals(Math.roundTo(4.1235343424, 6), 4.123534);
+    Test.assertEquals(Math.roundTo(5.3035802, 3), 5.304);
+    Test.assertEquals(Math.roundTo(0.9384930193848595, 15), 0.93849301938486);
+    Test.assertEquals(Math.roundTo(9.9999, 3), 10);
+  });
+});
+*/
