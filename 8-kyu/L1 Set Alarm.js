de17fcd63cd633d07ea8805944b327c0642a4c56
satyadvaya <satyadvaya@gmail.com>
2023-05-03 14:00:00 -0700

    refactored challenge

--- 8-kyu/L1 Set Alarm.js
+++ 8-kyu/L1 Set Alarm.js
@@ -1,27 +1,31 @@
-// Description:
+/*
+Description:
 
-// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
 
-// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
 
-// setAlarm(true, true) -> false
-// setAlarm(false, true) -> false
-// setAlarm(false, false) -> false
-// setAlarm(true, false) -> true
+setAlarm(true, true) -> false
+setAlarm(false, true) -> false
+setAlarm(false, false) -> false
+setAlarm(true, false) -> true
+*/
 
 // eslint-disable-next-line no-unused-vars
 function setAlarm(employed, vacation) {
   return employed && !vacation;
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Test Suite",()=>{
-//   it("Fixed tests",()=>{
-//     assert.strictEqual(setAlarm(true, true),  false,"Should be false.");
-//     assert.strictEqual(setAlarm(false,true),  false, "Should be false.");
-//     assert.strictEqual(setAlarm(true, false), true,"Should be true.");
-//   });
-// });
+describe("Test Suite", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(setAlarm(true, true), false, "Should be false.");
+    assert.strictEqual(setAlarm(false, true), false, "Should be false.");
+    assert.strictEqual(setAlarm(true, false), true, "Should be true.");
+  });
+});
+*/
