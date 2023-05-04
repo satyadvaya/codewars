eb9132d0e068add008d8d9b8010658f6c4f87208
satyadvaya <satyadvaya@gmail.com>
2023-05-03 17:00:00 -0700

    refactored challenge

--- 8-kyu/Logical Calculator.js
+++ 8-kyu/Logical Calculator.js
@@ -1,35 +1,37 @@
-// Description:
-// Your Task
+/*
+Description:
+Your Task
 
-// Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
-// Examples
+Given an array of Boolean values and a logical operator, return a Boolean result based on sequentially applying the operator to the values in the array.
+Examples
 
-//     booleans = [True, True, False], operator = "AND"
+    booleans = [True, True, False], operator = "AND"
 
-//     True AND True -> True
-//     True AND False -> False
-//     return False
+    True AND True -> True
+    True AND False -> False
+    return False
 
-//     booleans = [True, True, False], operator = "OR"
+    booleans = [True, True, False], operator = "OR"
 
-//     True OR True -> True
-//     True OR False -> True
-//     return True
+    True OR True -> True
+    True OR False -> True
+    return True
 
-//     booleans = [True, True, False], operator = "XOR"
+    booleans = [True, True, False], operator = "XOR"
 
-//     True XOR True -> False
-//     False XOR False -> False
-//     return False
+    True XOR True -> False
+    False XOR False -> False
+    return False
 
-// Input
+Input
 
-//     an array of Boolean values (1 <= array_length <= 50)
-//     a string specifying a logical operator: "AND", "OR", "XOR"
+    an array of Boolean values (1 <= array_length <= 50)
+    a string specifying a logical operator: "AND", "OR", "XOR"
 
-// Output
+Output
 
-// A Boolean value (True or False).
+A Boolean value (True or False).
+*/
 
 // eslint-disable-next-line no-unused-vars
 function logicalCalc(array, op) {
@@ -38,31 +40,33 @@ function logicalCalc(array, op) {
   );
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
+/*
+const chai = require("chai");
+const assert = chai.assert;
 
-// describe("Testing", function() {
-//   it("Fixed Tests", function() {
-//     assert.strictEqual(logicalCalc([true, true, true, false], "AND"), false);
-//     assert.strictEqual(logicalCalc([true, true, true, false], "OR"), true);
-//     assert.strictEqual(logicalCalc([true, true, true, false], "XOR"), true);
-//     assert.strictEqual(logicalCalc([true, true, false, false], "AND"), false);
-//     assert.strictEqual(logicalCalc([true, true, false, false], "OR"), true);
-//     assert.strictEqual(logicalCalc([true, true, false, false], "XOR"), false);
-//     assert.strictEqual(logicalCalc([true, false, false, false], "AND"), false);
-//     assert.strictEqual(logicalCalc([true, false, false, false], "OR"), true);
-//     assert.strictEqual(logicalCalc([true, false, false, false], "XOR"), true);
-//     assert.strictEqual(logicalCalc([true, true], "AND"), true);
-//     assert.strictEqual(logicalCalc([true, true], "OR"), true);
-//     assert.strictEqual(logicalCalc([true, true], "XOR"), false);
-//     assert.strictEqual(logicalCalc([false, false], "AND"), false);
-//     assert.strictEqual(logicalCalc([false, false], "OR"), false);
-//     assert.strictEqual(logicalCalc([false, false], "XOR"), false);
-//     assert.strictEqual(logicalCalc([false], "AND"), false);
-//     assert.strictEqual(logicalCalc([false], "OR"), false);
-//     assert.strictEqual(logicalCalc([false], "XOR"), false);
-//     assert.strictEqual(logicalCalc([true], "AND"), true);
-//     assert.strictEqual(logicalCalc([true], "OR"), true);
-//     assert.strictEqual(logicalCalc([true], "XOR"), true);
-//   });
-// });
+describe("Testing", function () {
+  it("Fixed Tests", function () {
+    assert.strictEqual(logicalCalc([true, true, true, false], "AND"), false);
+    assert.strictEqual(logicalCalc([true, true, true, false], "OR"), true);
+    assert.strictEqual(logicalCalc([true, true, true, false], "XOR"), true);
+    assert.strictEqual(logicalCalc([true, true, false, false], "AND"), false);
+    assert.strictEqual(logicalCalc([true, true, false, false], "OR"), true);
+    assert.strictEqual(logicalCalc([true, true, false, false], "XOR"), false);
+    assert.strictEqual(logicalCalc([true, false, false, false], "AND"), false);
+    assert.strictEqual(logicalCalc([true, false, false, false], "OR"), true);
+    assert.strictEqual(logicalCalc([true, false, false, false], "XOR"), true);
+    assert.strictEqual(logicalCalc([true, true], "AND"), true);
+    assert.strictEqual(logicalCalc([true, true], "OR"), true);
+    assert.strictEqual(logicalCalc([true, true], "XOR"), false);
+    assert.strictEqual(logicalCalc([false, false], "AND"), false);
+    assert.strictEqual(logicalCalc([false, false], "OR"), false);
+    assert.strictEqual(logicalCalc([false, false], "XOR"), false);
+    assert.strictEqual(logicalCalc([false], "AND"), false);
+    assert.strictEqual(logicalCalc([false], "OR"), false);
+    assert.strictEqual(logicalCalc([false], "XOR"), false);
+    assert.strictEqual(logicalCalc([true], "AND"), true);
+    assert.strictEqual(logicalCalc([true], "OR"), true);
+    assert.strictEqual(logicalCalc([true], "XOR"), true);
+  });
+});
+*/
