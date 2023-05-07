78abeb66248f02fe61b305ce0fec48ee20a086d1
satyadvaya <satyadvaya@gmail.com>
2023-05-07 10:00:00 -0700

    refactored challenge

--- 8-kyu/Merge Two Sorted Arrays into One.js
+++ 8-kyu/Merge Two Sorted Arrays into One.js
@@ -1,26 +1,43 @@
-// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+/*
+Description:
 
-// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
 
-// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 
-// Examples (input -> output)
-// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
 
-// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Examples (input -> output)
+* [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
-// Happy coding!
+* [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+* [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
+Happy coding!*/
 
 // eslint-disable-next-line no-unused-vars
 function mergeArrays(arr1, arr2) {
   return [...new Set([...arr1, ...arr2].sort((a, b) => a - b))];
 }
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Test.assertSimilar(mergeArrays([1,2,3,4], [5,6,7,8]), [1,2,3,4,5,6,7,8], "Basic tests");
-// Test.assertSimilar(mergeArrays([1,3,5,7,9], [10,8,6,4,2]), [1,2,3,4,5,6,7,8,9,10], "Basic tests");
-// Test.assertSimilar(mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12]), [1,2,3,4,5,7,9,10,11,12], "Basic tests");
-//   });
-// });
+/*
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertSimilar(
+      mergeArrays([1, 2, 3, 4], [5, 6, 7, 8]),
+      [1, 2, 3, 4, 5, 6, 7, 8],
+      "Basic tests"
+    );
+    Test.assertSimilar(
+      mergeArrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]),
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      "Basic tests"
+    );
+    Test.assertSimilar(
+      mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]),
+      [1, 2, 3, 4, 5, 7, 9, 10, 11, 12],
+      "Basic tests"
+    );
+  });
+});
+*/
