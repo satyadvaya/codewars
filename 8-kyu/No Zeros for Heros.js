b1abba3eefd69e524f0899c739f51d25ac2aa760
satyadvaya <satyadvaya@gmail.com>
2023-05-09 21:42:50 -0700

    refactored challenge

--- 8-kyu/No Zeros for Heros.js
+++ 8-kyu/No Zeros for Heros.js
@@ -1,17 +1,19 @@
-// Description:
+/*
+Description:
 
-// Numbers ending with zeros are boring.
+Numbers ending with zeros are boring.
 
-// They might be fun in your world, but not here.
+They might be fun in your world, but not here.
 
-// Get rid of them. Only the ending ones.
+Get rid of them. Only the ending ones.
 
-// 1450 -> 145
-// 960000 -> 96
-// 1050 -> 105
-// -1050 -> -105
+1450 -> 145
+960000 -> 96
+1050 -> 105
+-1050 -> -105
 
-// Zero alone is fine, don't worry about it. Poor guy anyway
+Zero alone is fine, don't worry about it. Poor guy anyway
+*/
 
 // eslint-disable-next-line no-unused-vars
 function noBoringZeros(n) {
@@ -22,19 +24,21 @@ function noBoringZeros(n) {
   return n;
 }
 
-// const Test = require('@codewars/test-compat');
+/*
+const Test = require("@codewars/test-compat");
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests",function() {
-//   it("noBoringZeros",function() {
-//     assert.strictEqual(noBoringZeros(1450), 145)
-//     assert.strictEqual(noBoringZeros(960000), 96)
-//     assert.strictEqual(noBoringZeros(1050), 105)
-//     assert.strictEqual(noBoringZeros(-1050), -105)
-//     assert.strictEqual(noBoringZeros(-105), -105)
-//     assert.strictEqual(noBoringZeros(0), 0)
-//   })
-// })
+describe("Basic tests", function () {
+  it("noBoringZeros", function () {
+    assert.strictEqual(noBoringZeros(1450), 145);
+    assert.strictEqual(noBoringZeros(960000), 96);
+    assert.strictEqual(noBoringZeros(1050), 105);
+    assert.strictEqual(noBoringZeros(-1050), -105);
+    assert.strictEqual(noBoringZeros(-105), -105);
+    assert.strictEqual(noBoringZeros(0), 0);
+  });
+});
+*/
