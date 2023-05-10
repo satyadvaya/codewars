7c746ee02fa85a829d464af06ad1a12f4a4b98dc
satyadvaya <satyadvaya@gmail.com>
2023-05-09 21:51:46 -0700

    refactored challenge

--- 8-kyu/Not So Black Box.js
+++ 8-kyu/Not So Black Box.js
@@ -1,18 +1,20 @@
-// Description:
+/*
+Description:
 
-// The kata has been preloaded with a function called blackBox. Figure out what the function does.
+The kata has been preloaded with a function called blackBox. Figure out what the function does.
 
-// You may need to think laterally!
+You may need to think laterally!
+*/
 
 // eslint-disable-next-line no-undef
 console.log(blackBox.toString());
-// if("you call JSopenSesame(), you'll pass!");
+// if ("you call JSopenSesame(), you'll pass!");
 
 // eslint-disable-next-line no-undef
 JSopenSesame();
 
-// describe("Did you open the box?", function() {
-
-//   Test.expect(check(), "It's clamped shut!");
-
-// });
+/*
+describe("Did you open the box?", function () {
+  Test.expect(check(), "It's clamped shut!");
+});
+*/
