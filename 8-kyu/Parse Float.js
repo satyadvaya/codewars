e53e78111ffa8a89d12ae600ac8bd042164276ac
satyadvaya <satyadvaya@gmail.com>
2023-05-10 23:44:02 -0700

    refactored challenge

--- 8-kyu/Parse Float.js
+++ 8-kyu/Parse Float.js
@@ -1,14 +1,18 @@
-// Description:
+/*
+Description:
 
-// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+*/
 
 // eslint-disable-next-line no-unused-vars
 function parseF(s) {
   return Number.isNaN(parseFloat(s)) ? null : parseFloat(s);
 }
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Test.assertEquals(parseF("1"), 1.0);
-//   });
-// });
+/*
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(parseF("1"), 1.0);
+  });
+});
+*/
