68d9033870697e56664cf0645a5a21842cec66f8
satyadvaya <satyadvaya@gmail.com>
2023-05-13 23:41:54 -0700

    refactored challenge

--- 8-kyu/Pick a Set of First Elements.js
+++ 8-kyu/Pick a Set of First Elements.js
@@ -1,26 +1,29 @@
-// Description:
+/*
+Description:
 
-// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
 
-// If n == 0 return an empty sequence []
-// Examples
+If n == 0 return an empty sequence []
+Examples
 
-// var arr = ['a', 'b', 'c', 'd', 'e'];
-// first(arr) //=> ['a'];
-// first(arr, 2) //=> ['a', 'b']
-// first(arr, 3) //=> ['a', 'b', 'c'];
-// first(arr, 0) //=> [];
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
 
 // eslint-disable-next-line no-unused-vars
 function first(arr, n) {
   return n === 0 ? [] : arr.slice(0, n > 0 ? n : 1);
 }
 
-// describe("Tests", () => {
-//   it("test", () => {
-// var arr = ['a', 'b', 'c', 'd', 'e'];
-// Test.assertSimilar(first(arr), ['a']);
-// Test.assertSimilar(first(arr, 2), ['a', 'b']);
-
-//   });
-// });
+/*
+describe("Tests", () => {
+  it("test", () => {
+    var arr = ["a", "b", "c", "d", "e"];
+    Test.assertSimilar(first(arr), ["a"]);
+    Test.assertSimilar(first(arr, 2), ["a", "b"]);
+  });
+});
+*/
