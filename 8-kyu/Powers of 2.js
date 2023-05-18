d513fa911f734365bc7eba20f7c796d4d6b77f10
satyadvaya <satyadvaya@gmail.com>
2023-05-17 23:54:18 -0700

    refactored challenge

--- 8-kyu/Powers of 2.js
+++ 8-kyu/Powers of 2.js
@@ -1,11 +1,13 @@
-// Description:
+/*
+Description:
 
-// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
-// Examples
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+Examples
 
-// n = 0  ==> [1]        # [2^0]
-// n = 1  ==> [1, 2]     # [2^0, 2^1]
-// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
 
 // eslint-disable-next-line no-unused-vars
 function powersOfTwo(n) {
@@ -16,14 +18,16 @@ function powersOfTwo(n) {
   return array;
 }
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+/*
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic Tests", function(){
-//   it("Testing for fixed tests", () => {
-//     assert.deepEqual(powersOfTwo(0), [1])
-//     assert.deepEqual(powersOfTwo(1), [1, 2])
-//     assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16])
-//   })
-// });
+describe("Basic Tests", function () {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(powersOfTwo(0), [1]);
+    assert.deepEqual(powersOfTwo(1), [1, 2]);
+    assert.deepEqual(powersOfTwo(4), [1, 2, 4, 8, 16]);
+  });
+});
+*/
