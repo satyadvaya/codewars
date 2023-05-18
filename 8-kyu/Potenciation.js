4c7a7c73014b12f48ad1d321fa2086e6d231914a
satyadvaya <satyadvaya@gmail.com>
2023-05-17 23:53:06 -0700

    refactored challenge

--- 8-kyu/Potenciation.js
+++ 8-kyu/Potenciation.js
@@ -1,23 +1,26 @@
-// DESCRIPTION:
-// The function takes in 2 inputs x and y, and should return x to the power of y
+/*
+DESCRIPTION:
+The function takes in 2 inputs x and y, and should return x to the power of y
 
-// Simple, right? But you're NOT allowed to use Math.pow();
+Simple, right? But you're NOT allowed to use Math.pow();
 
-// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+Obs: x and y will be naturals, so DON'T take fractions into consideration;
 
-// Note : zero to the power of zero equals one in this kata
+Note : zero to the power of zero equals one in this kata
 
-// Great coding <3
+Great coding <3
+*/
 
+// eslint-disable-next-line no-unused-vars
 function power(x, y) {
   return x ** y;
 }
 
-// describe("Tests", function(){
-
-// Test.assertEquals(power(1,701270), 1);
-// Test.assertEquals(power(2,2), 4);
-// Test.assertEquals(power(3,2), 9);
-// Test.assertEquals(power(-1,40), 1);
-
-// });
+/*
+describe("Tests", function () {
+  Test.assertEquals(power(1, 701270), 1);
+  Test.assertEquals(power(2, 2), 4);
+  Test.assertEquals(power(3, 2), 9);
+  Test.assertEquals(power(-1, 40), 1);
+});
+*/
