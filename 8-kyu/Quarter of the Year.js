9dbf80fa9f534b0d124d760301de119a5e4d0ee4
satyadvaya <satyadvaya@gmail.com>
2023-05-19 10:00:00 -0700

    refactored challenge

--- 8-kyu/Quarter of the Year.js
+++ 8-kyu/Quarter of the Year.js
@@ -1,24 +1,28 @@
-// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
 
-// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+*/
 
+// eslint-disable-next-line no-unused-vars
 const quarterOf = (month) => Math.ceil(month / 3);
 
-// const quarterOf = (month) => {
-//   return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
-// };
+/*
+const quarterOf = (month) => {
+  return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
+};
 
-// const quarterOf = (month) => {
-//   if (month < 4) {
-//     return 1;
-//   } else if (month < 7) {
-//     return 2;
-//   } else if (month < 10) {
-//     return 3;
-//   } else {
-//     return 4;
-//   }
-// };
+const quarterOf = (month) => {
+  if (month < 4) {
+    return 1;
+  } else if (month < 7) {
+    return 2;
+  } else if (month < 10) {
+    return 3;
+  } else {
+    return 4;
+  }
+};
 
 const chai = require("chai");
 const assert = chai.assert;
@@ -31,3 +35,4 @@ describe("Basic Tests", () => {
     assert.strictEqual(quarterOf(11), 4);
   });
 });
+*/
