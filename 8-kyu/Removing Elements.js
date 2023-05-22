d07bb9338a3ff0f48fe3105fe6944dbee683e26c
satyadvaya <satyadvaya@gmail.com>
2023-05-22 11:00:00 -0700

    refactored challenge

--- 8-kyu/Removing Elements.js
+++ 8-kyu/Removing Elements.js
@@ -1,34 +1,46 @@
-// Description:
+/*
+Description:
 
-// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
-// Example:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+Example:
 
-// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
-// None of the arrays will be empty, so you don't have to worry about that!
+None of the arrays will be empty, so you don't have to worry about that!
+*/
 
 // eslint-disable-next-line no-unused-vars
 function removeEveryOther(arr) {
   return arr.filter((element, index) => index % 2 === 0);
 }
 
-// function removeEveryOther(arr){
-//   let array = [];
-//   for (let i = 0; i < arr.length; i += 2) {
-//     array.push(arr[i]);
-//   }
-//   return array;
-// }
+/*
+function removeEveryOther(arr) {
+  let array = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    array.push(arr[i]);
+  }
+  return array;
+}
 
-// const chai = require("chai");
-// const assert = chai.assert;
-// chai.config.truncateThreshold=0;
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
 
-// describe("Basic tests",() =>{
-//   it("Testing for fixed tests", () => {
-//     assert.deepEqual(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
-//     assert.deepEqual(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
-//     assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
-//     assert.deepEqual(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
-//   })
-// });
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(removeEveryOther(["Hello", "Goodbye", "Hello Again"]), [
+      "Hello",
+      "Hello Again",
+    ]);
+    assert.deepEqual(
+      removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+      [1, 3, 5, 7, 9]
+    );
+    assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
+    assert.deepEqual(removeEveryOther([["Goodbye"], { Great: "Job" }]), [
+      ["Goodbye"],
+    ]);
+  });
+});
+*/
