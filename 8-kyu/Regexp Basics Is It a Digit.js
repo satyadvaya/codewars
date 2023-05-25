89b9714e2febb09d7215671717a52b5fc6e3ea36
satyadvaya <satyadvaya@gmail.com>
2023-05-25 14:41:12 -0700

    refactored challenge

--- 8-kyu/Regexp Basics Is It a Digit.js
+++ 8-kyu/Regexp Basics Is It a Digit.js
@@ -1,18 +1,22 @@
-// Description:
+/*
+Description:
 
-// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
+*/
 
 String.prototype.digit = function () {
   return /^\d$/.test(this);
 };
 
-// describe('hex number', () => {
-//   it('Basic tests', () => {
-//     Test.assertEquals(''.digit(), false);
-//     Test.assertEquals('7'.digit(), true);
-//     Test.assertEquals(' '.digit(), false);
-//     Test.assertEquals('a'.digit(), false);
-//     Test.assertEquals('a5'.digit(), false);
-//     Test.assertEquals('14'.digit(), false);
-//   });
-// });
+/*
+describe("hex number", () => {
+  it("Basic tests", () => {
+    Test.assertEquals("".digit(), false);
+    Test.assertEquals("7".digit(), true);
+    Test.assertEquals(" ".digit(), false);
+    Test.assertEquals("a".digit(), false);
+    Test.assertEquals("a5".digit(), false);
+    Test.assertEquals("14".digit(), false);
+  });
+});
+*/
