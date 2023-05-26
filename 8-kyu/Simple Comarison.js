cd495a2c9cdb86da332c8ebd1255029f82ff882d
satyadvaya <satyadvaya@gmail.com>
2023-05-26 11:00:00 -0700

    refactored challenge

--- 8-kyu/Simple Comarison.js
+++ 8-kyu/Simple Comarison.js
@@ -1,29 +1,33 @@
-// Description:
+/*
+Description:
 
-// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
 
-// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+*/
 
 // eslint-disable-next-line no-unused-vars
 function add(a, b) {
   return eval(a - b) == 0;
 }
 
-// function add(a, b) {
-//   return (a == b);
-// }
+/*
+function add(a, b) {
+  return a == b;
+}
 
-// function add(a, b) {
-//   return (+a == +b);
-// }
+function add(a, b) {
+  return +a == +b;
+}
 
-// describe("Tests", () => {
-//   it("test", () => {
-//     Test.assertEquals(add('1', 1), true);
-//     Test.assertEquals(add(1, '1'), true);
-//     Test.assertEquals(add(1, '0'), false);
-//     Test.assertEquals(add('11', 11), true);
-//     Test.assertEquals(add(12, 12), true);
-//     Test.assertEquals(add(120, '021'), false);
-//   });
-// });
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(add("1", 1), true);
+    Test.assertEquals(add(1, "1"), true);
+    Test.assertEquals(add(1, "0"), false);
+    Test.assertEquals(add("11", 11), true);
+    Test.assertEquals(add(12, 12), true);
+    Test.assertEquals(add(120, "021"), false);
+  });
+});
+*/
